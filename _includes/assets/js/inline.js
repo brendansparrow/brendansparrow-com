d660289fab5cf420fe4929c1e73af9b911160514
Brendan Sparrow <btsparrow@hotmail.co.uk>
2019-11-18 22:28:18 +0000

    Defer body scripts

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,9 +1,11 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
+window.addEventListener('load', function() {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+});
